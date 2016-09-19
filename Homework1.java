f67c34d27a1847ec8a49b8534eff7725f5cea6a2
Yasin Shuman <yshuman@loyalloops.com>
2016-09-18 22:16:11 -0700

    polished up formatting of text

--- Homework1.java
+++ Homework1.java
@@ -1,19 +1,20 @@
 /*
-  usage:
-  java Homework1 First Name Last Name
-  Expected Output: 
-  	Hi (First Name) (Last Name) the modulo of 19 and 2 is: 1
+  	Student: Yasin Shuman
+  	usage: java Homework1 First Name Last Name
+ 	Expected Output: 
+  		Hi (First Name) (Last Name) the modulo of 19 and 2 is: 1
 
-	Let's tab	over	twice here because I didnt know what else to do with the tabs.
+		Let's tab	over	twice here because I didnt know what else to do with the tabs. 
 */
 
 class Homework1	
 {	
 	public static void main(String[] args) 
 	{	
-		/*  Check if arguments are present
-		 	If arguments are present and formatted properly will proceed to doing math 
-		*/ 
+		/*  
+			Check if arguments are present
+		    If arguments are present and formatted properly will proceed to doing math 
+		*/
 		if (args.length == 0) {
 			System.out.print("You forgot to type in your first and last name when you ran this program." + "\n" + "This is the format you need: Java Homework1 (First Name) (Last Name)" + "\n" + "Go ahead and try it again.");
 			return;
@@ -22,7 +23,8 @@ class Homework1
 			return;
 	 	}
 
-		/*  takes two int variables a and b and finds the modulo and assigns it to variable c 
+		/*  
+			takes two int variables a and b and finds the modulo and assigns it to variable c 
 			it then prints out a message giving the user the modulo as well as demonstrating that i know how to use the escape sequences \n and \t 
 		*/
 		int a = learnMethod(9);
